7461276040c894fbe54533b7edebf751413badbd
ShibinaNoushad <shibinanoushad2224@gmail.com>
2022-11-06 19:58:35 +0530

    createdAt added

--- call_back.js
+++ call_back.js
@@ -1,13 +1,14 @@
 const posts=[
-    {title:"post one" ,des:"this is post one"},
-    {title:"post two" ,des:"this is post two"}
+    {title:"post one" ,des:"this is post one",createdAt: new Date().getTime()},
+    {title:"post two" ,des:"this is post two",createdAt: new Date().getTime()}
     
 ];
 function getPosts(){
     setTimeout(()=>{
         let output="";
         posts.forEach((post)=>{
-            output+=`<li>${post.title}</li>`
+            output+=`<li>${post.title} :created at ${post.createdAt}</li><br>`;
+
         });
         document.body.innerHTML=output;
 
@@ -21,4 +22,14 @@ function createPost(post,callback){
      },2000);
 };
 
-createPost({title:"post four", des:"this is post four"},getPosts);
+createPost({title:"post three", des:"this is post three",createdAt: new Date().getTime()},getPosts);
+
+
+function createFourthPost(post,callback){
+    setTimeout(()=>{
+        posts.push(post);
+        callback();
+    },3000);
+};
+
+createFourthPost({title:"post four",des:"this is post four",createdAt: new Date().getTime()},getPosts);
